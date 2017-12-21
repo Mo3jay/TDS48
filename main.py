3ac92c218119647fe9c5d4a6a024850fa60f8fbd
Moe <dcostello1021@gmail.com>
2017-12-20 21:10:48 -0500

    calc base on multiple points

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ class Point():
         self.y = y
         self.z = z
 
+    def __str__(self):
+        return "X: " + str(self.x) + ", Y: " + str(self.y)
+
 def calc_dist(x1, x2, y1, y2):
     x_pt = (x2 - x1) ** 2 
     y_pt = (y2 - y1) ** 2
@@ -15,16 +18,41 @@ def calc_ang(a, b, c):
     ang = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))
     return math.degrees(ang)
 
-northing = input("enter northing coord: ")
-easting = input("enter easting coord: ")
+def stakeout(setup):
+    oc_fs = calc_dist(setup["occupy"].x, setup["foresite"].x, setup["occupy"].y, setup["foresite"].y)
+    oc_bs = calc_dist(setup["occupy"].x, setup["backsite"].x, setup["occupy"].y, setup["backsite"].y)
+    bs_fs = calc_dist(setup["backsite"].x, setup["foresite"].x, setup["backsite"].y, setup["foresite"].y)
+    dist = oc_fs
+    ang = calc_ang(bs_fs, oc_bs, oc_fs)
+    return {"distance": dist, "angle": ang}
 
-occupy = Point(5000, 5000, 100)
-backsite = Point(4500, 4500, 90)
-foresite = Point(int(northing), int(easting))
+points = [
+    Point(5000, 5000),
+    Point(4678, 3290),
+    Point(6234, 3950),
+    Point(1337, 5093),
+    Point(3209, 1205),
+]
 
-c_dist = calc_dist(occupy.x, foresite.x, occupy.y, foresite.y)
-b_dist = calc_dist(occupy.x, backsite.x, occupy.y, backsite.y)
-a_dist = calc_dist(backsite.x, foresite.x, backsite.y, foresite.y)
+def display():
+    n = 1
+    for point in points:
+        print(n, "-", point)
+        n = n + 1
 
-print(c_dist)
-print(calc_ang(a_dist, b_dist, c_dist))
+def assign():
+    choice1 = input("select occupy: ")
+    choice2 = input("select backsite: ")
+    choice3 = input("select forsite: ")
+    setup = {
+        "occupy": points[int(choice1) - 1],
+        "backsite": points[int(choice2) - 1],
+        "foresite": points[int(choice3) - 1]
+    }
+    return setup
+
+    
+
+display()
+setup = assign()
+print(stakeout(setup))
